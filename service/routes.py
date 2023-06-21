8d9b8234ed6e230c5a596d87e20cfaab91dc84f8
amine9005 <amamselmitrader@gmail.com>
2023-06-21 16:11:33 +0100

    linting

--- service/routes.py
+++ service/routes.py
@@ -76,7 +76,7 @@ def list_accounts():
 
 
 ######################################################################
- # READ AN ACCOUNT
+# READ AN ACCOUNT
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
